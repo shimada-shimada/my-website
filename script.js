799ae4ca82ee325bf6a3e8dab663273126c2cb22
shimada-shimada <oyayubiotoko12345@gmail.com>
2026-06-08 08:59:44 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("showButton");
+const message = document.getElementById("message");
+
+button.addEventListener("click", function() {
+    message.innerHTML = "めっちゃチョコでめっちゃナッツだった <br>おいしかった <br>ありがとう <br>寝てる間冷蔵庫で冷やしとけばよかった";
+});
